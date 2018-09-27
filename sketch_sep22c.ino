ad75c3ddb0198a3c23efec00e8f0dc59131fa925
kimdoori <rlaenfl0228@gmail.com>
2018-09-27 13:49:27 +0900

    Update sketch_sep22c.ino

--- sketch_sep22c.ino
+++ sketch_sep22c.ino
@@ -3,28 +3,43 @@
 
 double jump_value;
 double down_value;
+
+bool jump_flag = false;
+bool down_flag = false;
+
 void setup()
 {
   Serial.begin(9600);
 }
 void loop()
 {
-  jump_value = analogRead(A0);
   
+  
+  jump_value = analogRead(A0);
   down_value = analogRead(A1);
   
   Serial.println(jump_value);
   Serial.println(down_value);
 
   if(jump_value<=650){
+        if(!jump_flag){
+          //함수호출
+        }
+        jump_flag = true;
         digitalWrite(LED_PIN1, HIGH);
   }else{
+        jump_flag = false;
         digitalWrite(LED_PIN1, LOW);
   }
 
   if(down_value<=120){
+        if(!down_flag){
+          //함수호출
+        }
+        down_flag = true;
         digitalWrite(LED_PIN2, HIGH);
   }else{
+        down_flag = false;
         digitalWrite(LED_PIN2, LOW);
   }
 
